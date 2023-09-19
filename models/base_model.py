5ddb77d7de47a128d683d85c06cd6fb56122c15b
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-19 17:10:16 +0100

    updated: models/base_model.py, models/city.py, state

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,17 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base
 
 
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Initialize the BaseModel class.
@@ -18,8 +25,8 @@ class BaseModel:
         """
         # public instance attributes
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         if len(kwargs):
             iso_format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -34,7 +41,7 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute updated_at \
             with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
@@ -45,8 +52,13 @@ class BaseModel:
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         dict_copy["__class__"] = self.__class__.__name__
+        dict_copy.pop('_sa_instance_state', None)
 
         return (dict_copy)
+
+    def delete(self):
+        """Delete current instance from storage."""
+        models.storage.delete(self)
 
     def __str__(self):
         """Return the string representation of the instance."""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, DateTime, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'State'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='delete', backref='State')
+
+    # if os.getenv("HBNB_TYPE_STORAGE") != "db":
+    @property
+    def cities(self):
+        """Return list of related City objects."""
+        return [city for city in models.storage.all('City').values()
+                if city.state_id == self.id]
