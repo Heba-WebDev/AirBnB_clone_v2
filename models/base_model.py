cc11b1b3872f7bb90d8db62b388ebf049c82ebc9
Heba Omar <jtvonline@yahoo.com>
2023-09-20 00:32:25 +0300

    Update BaseModel, Create Base = declarative_base() before the class definition of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -25,17 +25,13 @@ class BaseModel:
         """
         # public instance attributes
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        if len(kwargs):
-            iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
-                if key in ["created_at", "updated_at"]:
-                    self.__dict__[key] = datetime.strptime(value, iso_format)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     # public instance methods
     def save(self):
@@ -51,7 +47,7 @@ class BaseModel:
         dict_copy = self.__dict__.copy()
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
-        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["__class__"] = str(type(self).__name__)
         dict_copy.pop('_sa_instance_state', None)
 
         return (dict_copy)
@@ -62,4 +58,6 @@ class BaseModel:
 
     def __str__(self):
         """Return the string representation of the instance."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        dict = self.__dict__.copy()
+        dict.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, dict)
