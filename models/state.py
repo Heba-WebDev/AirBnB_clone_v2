ed025199bff546a30d1299f3476c9bfd0e5aa78d
Heba Omar <jtvonline@yahoo.com>
2023-09-20 00:46:32 +0300

    Update State, State inherits from BaseModel and Base

--- models/state.py
+++ models/state.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, DateTime, String, ForeignKey
 from sqlalchemy.orm import relationship
-import os
-import models
+from models.city import City
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'State'
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='delete', backref='State')
 
     # if os.getenv("HBNB_TYPE_STORAGE") != "db":
-    @property
-    def cities(self):
-        """Return list of related City objects."""
-        return [city for city in models.storage.all('City').values()
-                if city.state_id == self.id]
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Return list of related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
