052def6d9d954af5ba4d71ab7cd8099c1a1a7cbf
Heba Omar <jtvonline@yahoo.com>
2023-09-20 16:48:34 +0300

    Updates for Task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,73 +1,70 @@
 #!/usr/bin/python3
-"""The new engine DBStorage"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import scoped_session
+"""Database storage"""
+
 from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.city import City
-from models.place import Place
-from models.review import Review
+from models.amenity import Amenity
 from models.state import State
+from models.place import Place
 from models.user import User
+from models.review import Review
 
 
-class DBStorage:
-    """Class definition"""
+class DBStorage():
+    """This is the database storage class"""
+
     __engine = None
     __session = None
 
     def __init__(self):
-        """Creates a new DB"""
+        """This is the constructor"""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
             getenv("HBNB_MYSQL_USER"),
             getenv("HBNB_MYSQL_PWD"),
             getenv("HBNB_MYSQL_HOST"),
-            getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
+            getenv("HBNB_MYSQL_DB")
+        ), pool_pre_ping=True)
 
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """"""
+        """This is used to query from the data base"""
+        objects = []
+        model_names = [State, City]
         if cls is None:
-            objs = self.__session.query(State).all()
-            objs.extend(self.__session.query(City).all())
-            objs.extend(self.__session.query(User).all())
-            objs.extend(self.__session.query(Place).all())
-            objs.extend(self.__session.query(Review).all())
-            objs.extend(self.__session.query(Amenity).all())
+            for i in model_names:
+                if inspect(self.__engine).has_table(i.__tablename__):
+                    objects.extend(self.__session.query(i).all())
         else:
             if type(cls) == str:
-                cls = eval(cls)
-            objs = self.__session.query(cls)
-
-        return {"{}.{}".format(
-            type(obj).__name__, obj.id): obj for obj in objs}
+                try:
+                    cls = eval(cls)
+                except Exception:
+                    pass
+            objects = self.__session.query(cls).all()
+        return {f"{type(i).__name__}.{i.id}": i for i in objects}
 
     def new(self, obj):
         """Adds obj to the database"""
-        self.__session.add(obj)
+        self.__session.merge(obj)
 
     def save(self):
-        """Commits all changes to the database"""
+        """Saves to the database"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Deletes obj from the database"""
+        """Deletes from the database"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Creates all tables in the database"""
+        """Refreshes the database"""
         Base.metadata.create_all(self.__engine)
-        session_def = sessionmaker(bind=self.__engine,
-                                   expire_on_commit=False)
-        Session = scoped_session(session_def)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
         self.__session = Session()
-
-    def close(self):
-        """Closes current session"""
-        self.__session.close()
