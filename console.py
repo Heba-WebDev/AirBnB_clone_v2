f93319e7fbb8d54a12ca4cdb3d7927d5ce504ebc
Abubakar Adam Sani <Athernee@gmail.com>
2023-09-20 21:16:27 +0100

    fixed default method

--- console.py
+++ console.py
@@ -191,54 +191,47 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
 
-    # def strip_clean(self, args):
-    #     """strips the argument and return a string of command
-    #     Args:
-    #         args: input list of args
-    #     Return:
-    #         returns string of argumetns
-    #     """
-    #     new_list = []
-    #     new_list.append(args[0])
-    #     try:
-    #         my_dict = eval(
-    #             args[1][args[1].find('{'):args[1].find('}')+1])
-    #     except Exception:
-    #         my_dict = None
-    #     if isinstance(my_dict, dict):
-    #         new_str = args[1][args[1].find('(')+1:args[1].find(')')]
-    #         new_list.append(((new_str.split(", "))[0]).strip('"'))
-    #         new_list.append(my_dict)
-    #         return new_list
-    #     new_str = args[1][args[1].find('(')+1:args[1].find(')')]
-    #     new_list.append(" ".join(new_str.split(", ")))
-    #     return " ".join(i for i in new_list)
+    def default(self, line):
+        """Process instances of a class."""
+        my_list = line.split('.')
+        if len(my_list) >= 2:
+            action = my_list[1]
+            args = self._parse(my_list)
 
-    # def default(self, line):
-    #     """retrieve all instances of a class and
-    #     retrieve the number of instances
-    #     """
-    #     my_list = line.split('.')
-    #     if len(my_list) >= 2:
-    #         if my_list[1] == "all()":
-    #             self.do_all(my_list[0])
-    #         elif my_list[1] == "count()":
-    #             self.count(my_list[0])
-    #         elif my_list[1][:4] == "show":
-    #             self.do_show(self.strip_clean(my_list))
-    #         elif my_list[1][:7] == "destroy":
-    #             self.do_destroy(self.strip_clean(my_list))
-    #         elif my_list[1][:6] == "update":
-    #             args = self.strip_clean(my_list)
-    #             if isinstance(args, list):
-    #                 obj = storage.all()
-    #                 key = args[0] + ' ' + args[1]
-    #                 for k, v in args[2].items():
-    #                     self.do_update(key + ' "{}" "{}"'.format(k, v))
-    #             else:
-    #                 self.do_update(args)
-    #     else:
-    #         cmd.Cmd.default(self, line)
+            if action == "all()":
+                self.do_all(my_list[0])
+            elif action == "count()":
+                self.count(my_list[0])
+            elif action.startswith("show"):
+                self.do_show(args)
+            elif action.startswith("destroy"):
+                self.do_destroy(args)
+            elif action.startswith("update"):
+                if isinstance(args, list):
+                    obj = storage.all()
+                    key = args[0] + ' ' + args[1]
+                    for k, v in args[2].items():
+                        self.do_update(key + ' "{}" "{}"'.format(k, v))
+                else:
+                    self.do_update(args)
+        else:
+            cmd.Cmd.default(self, line)
+
+    def _parse(self, args):
+        """Strip given arguments to return a string."""
+        new_list = [args[0]]
+        try:
+            my_dict = eval(args[1][args[1].find('{'):args[1].find('}') + 1])
+        except Exception:
+            my_dict = None
+
+        new_str = args[1][args[1].find('(') + 1:args[1].find(')')]
+        if isinstance(my_dict, dict):
+            new_list.extend([(new_str.split(", "))[0].strip('"'), my_dict])
+        else:
+            new_list.append(" ".join(new_str.split(", ")))
+
+        return " ".join(new_list)
 
 
 if __name__ == '__main__':
