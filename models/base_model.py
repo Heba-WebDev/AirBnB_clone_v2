132231acbc46d08d70f8a41d7ba7c4d5a7b2a09a
Heba Omar <jtvonline@yahoo.com>
2023-09-20 16:18:07 +0300

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import models
 from sqlalchemy import Column, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 Base = declarative_base()
 
@@ -24,14 +25,23 @@ class BaseModel:
             kwargs (dict): dictionary of key/value pairs attributes
         """
         # public instance attributes
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key in kwargs:
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.fromisoformat(kwargs[key]))
+                elif key != '__class__':
+                    setattr(self, key, kwargs[key])
+            if getenv('HBNB_TYPE_STORAGE') == 'db':
+                if not hasattr(kwargs, 'id'):
+                    setattr(self, 'id', str(uuid.uuid4()))
+                if not hasattr(kwargs, 'created_at'):
+                    setattr(self, 'created_at', datetime.now())
+                if not hasattr(kwargs, 'updated_at'):
+                    setattr(self, 'updated_at', datetime.now())
 
     # public instance methods
     def save(self):
@@ -45,10 +55,12 @@ class BaseModel:
         """Returns a dictionary containing all \
             keys/values of __dict__ of the instance."""
         dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = str(type(self).__name__)
-        dict_copy["created_at"] = self.created_at.isoformat()
-        dict_copy["updated_at"] = self.updated_at.isoformat()
-        dict_copy.pop("_sa_instance_state", None)
+        dict_copy['__class__'] = self.__class__.__name__
+        for key in dict_copy:
+            if type(dict_copy[key]) is datetime:
+                dict_copy[key] = dict_copy[key].isoformat()
+        if '_sa_instance_state' in dict_copy.keys():
+            del(dict_copy['_sa_instance_state'])
         return dict_copy
 
     def delete(self):
