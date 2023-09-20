31d8ac481c9b0301f4de4df16f2684fd0214ae10
Heba Omar <jtvonline@yahoo.com>
2023-09-20 16:36:59 +0300

    Update City

--- models/city.py
+++ models/city.py
@@ -10,10 +10,11 @@ import os
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        places = relationship('Place', cascade='delete', backref='cities')
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     else:
         name = ''
         state_id = ''
