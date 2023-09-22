fedafe971c5ac49c6209ffe20c7c2850809833e0
Heba Omar <74996096+Heba-WebDev@users.noreply.github.com>
2023-09-22 22:04:45 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
