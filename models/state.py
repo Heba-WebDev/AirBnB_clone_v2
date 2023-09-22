b4dec6fd61a57241117a99f6eb96f063f34ff3d7
Heba Omar <74996096+Heba-WebDev@users.noreply.github.com>
2023-09-22 22:04:04 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+""" State Module """
 import models
 from models.city import City
-import shlex
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, DateTime, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    cities = relationship("City", backref="state", cascade="all, delete")
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter for cities"""
+            cities_list = [i for i in list(models.storage.all(City).values())
+                           if i.state_id == self.id]
+            return cities_list
