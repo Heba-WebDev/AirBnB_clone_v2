cc50af7b952f5d530f806469a3d369e77682e457
Heba Omar <jtvonline@yahoo.com>
2023-10-23 01:05:39 +0300

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,90 +1,44 @@
 #!/usr/bin/python3
-""" States and State """
-from flask import Flask
-from flask import render_template
+""" imports """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """ Home """
-    return "Hello HBNB!"
+@app.route("/states", strict_slashes=False)
+def states():
+    """
+    hbnb
+    """
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    return render_template("9-states.html", states=states)
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """ HBNB """
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """ C """
-    return 'c {}'.format(text.replace("_", " "))
-
-
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """ Python """
-    return 'Python {}'.format(text.replace("_", " "))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ Number """
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """ Even or Odd """
-    if n % 2 == 0:
-        p = 'even'
-    else:
-        p = 'odd'
-    return render_template('6-number_odd_or_even.html', number=n, parity=p)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ States """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_list():
-    """ Cities """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states(id=None):
-    """ Route function for /states and /states/<id> """
-    not_found = False
-    if id is not None:
-        states = storage.all(State, id)
-        with_id = True
-        if len(states) == 0:
-            not_found = True
-    else:
-        states = storage.all(State)
-        with_id = False
-    return render_template('9-states.html', states=states,
-                           with_id=with_id, not_found=not_found)
+@app.route("/states/<id>", strict_slashes=False)
+def cities_by_state_id(id):
+    """
+    hbnb
+    """
+    state_id = None
+    for i in list(storage.all(State).values()):
+        if i.id == id:
+            state_id = i
+    return render_template("9-states.html", state_id=state_id)
 
 
 @app.teardown_appcontext
-def close(error):
-    """ Close DB """
+def teardown(exception):
+    """
+    teardown app context
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    """
+    main
+    """
+    app.run(host='0.0.0.0', port=5000)
