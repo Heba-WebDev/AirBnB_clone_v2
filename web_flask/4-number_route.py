df6627fdf90751ceb110972946534cafb9af0f33
Heba Omar <jtvonline@yahoo.com>
2023-10-22 18:24:56 +0300

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Is it a number? """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def homepage():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_page(text):
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_page(text):
+    text = text.replace('_', ' ')
+    return ("Python {}".format(text))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_page(n):
+    return ("{} is a number".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
