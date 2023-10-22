0a9c717e37fdbc8188680a57eb9833303d026c0a
Heba Omar <jtvonline@yahoo.com>
2023-10-22 18:06:58 +0300

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,9 +13,11 @@ def homepage():
 def hbnb_page():
     return "HBNB"
 
+
 @app.route("/c/{value}", strict_slashes=False)
 def c_page():
     return "C is {value}"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
