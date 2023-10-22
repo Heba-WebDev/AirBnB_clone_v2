51a391737a3472f92598374e928549762537069a
Heba Omar <jtvonline@yahoo.com>
2023-10-23 01:43:47 +0300

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
-""" imports """
-from flask import Flask, render_template
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
-from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
 def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
     """
-    hbnb
-    """
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
-    return render_template("9-states.html", states=states)
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
-def cities_by_state_id(id):
-    """
-    hbnb
-    """
-    state_id = None
-    for i in list(storage.all(State).values()):
-        if i.id == id:
-            state_id = i
-    return render_template("9-states.html", state_id=state_id)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown app context
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    """
-    main
-    """
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
